13bff20e7a89bb252dbe2601d8dd30caf5b0aea3
andy179176 <andy179176@gmail.com>
2024-10-22 19:04:42 +0200

    change store from array to list

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,23 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dictionary {
-   Word[] words;
+   //Word[] words;
+   private List<Word> words = new ArrayList<>();
+
 
     public Dictionary(Word[] words) {
-        this.words = words;
+        //this.words = words;
+        for (int i =0; i<words.length; i++){
+            this.words.add(words[i]);
+        }
     }
 
     public String translation(String value){
-        for (int i = 0; i<words.length; i++){
-            if (words[i].value.equalsIgnoreCase(value)){
-                return words[i].translation;
+        for (int i = 0; i<words.size(); i++){
+            if (words.get(i).value.equalsIgnoreCase(value)){
+                return words.get(i).translation;
             }
         }
         return "";
     }
 
     public String getWordByTranslation(String translation){
-        for (int i = 0; i<words.length; i++){
-            if (words[i].translation.equalsIgnoreCase(translation)){
-                return words[i].value;
+        for (int i = 0; i<words.size(); i++){
+            if (words.get(i).translation.equalsIgnoreCase(translation)){
+                return words.get(i).value;
             }
         }
         return "";
@@ -25,8 +33,8 @@ public class Dictionary {
 
     public String toString(){
         String result="";
-        for (int i = 0; i < words.length; i++) {
-            result+= words[i]+ System.lineSeparator();  // \n
+        for (int i = 0; i < words.size(); i++) {
+            result+= words.get(i)+ System.lineSeparator();  // \n
         }
         return result;
     }
